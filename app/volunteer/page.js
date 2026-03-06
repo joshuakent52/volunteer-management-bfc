9f48da9f4a766fe939f9bf6d0b41cb719d22812d
joshuakent52 <joshuakent52@gmail.com>
2026-03-06 12:18:08 -0800

    Update page.js

--- app/volunteer/page.js
+++ app/volunteer/page.js
@@ -2,6 +2,8 @@
 import { useState, useEffect } from 'react'
 import { supabase } from '../../lib/supabase'
 
+export const dynamic = 'force-dynamic'
+
 export default function VolunteerPage() {
   const [user, setUser] = useState(null)
   const [profile, setProfile] = useState(null)
