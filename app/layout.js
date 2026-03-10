5c0c0e3d278239904a66ebaab7477c81f6687373
joshuakent52 <joshuakent52@gmail.com>
2026-03-10 11:43:41 -0600

    favicon addition

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import './globals.css'
 export const metadata = {
   title: 'BFC Volunteers',
   description: 'Volunteer Management Platform',
+  icons: {
+    icon: '/logo.jpg',
+  },
 }
 
 export default function RootLayout({ children }) {
