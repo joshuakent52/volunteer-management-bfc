6f45febc593bf61c8e57191523ad671d8c1ca9c7
joshuakent52 <joshuakent52@gmail.com>
2026-03-13 10:54:48 -0600

    Final change fixing improper logo branding.

--- app/layout.js
+++ app/layout.js
@@ -30,7 +30,7 @@ export default function RootLayout({ children }) {
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="apple-mobile-web-app-title" content="BFC" />
-        <link rel="apple-touch-icon" href="/logo.jpg" />
+        <link rel="apple-touch-icon" href="/logo2.png" />
         <link rel="manifest" href="/manifest.json" />
       </head>
       <body>
